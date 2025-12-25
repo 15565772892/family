322e46db42e65434407dab792949d3d74cc52edb
liyz <1411937456@qq.com>
2025-12-25 13:43:57 +0800

    增加log4J2

--- src/main/java/org/family/core/service/FamilyCoreServiceImpl.java
+++ src/main/java/org/family/core/service/FamilyCoreServiceImpl.java
@@ -1,5 +1,7 @@
 package org.family.core.service;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.family.core.dao.FamilyCoreDao;
 import org.family.core.dto.FamilyCoreDto;
 import org.family.core.util.DateUtils;
@@ -13,6 +15,8 @@ import java.util.List;
 @Service
 public class FamilyCoreServiceImpl implements FamilyCoreService {
 
+    private static final Logger logger = LogManager.getLogger(FamilyCoreServiceImpl.class);
+
     @Autowired
     private FamilyCoreDao familyCoreDao;
 
@@ -22,10 +26,33 @@ public class FamilyCoreServiceImpl implements FamilyCoreService {
     @Override
     public List<FamilyCoreDto> getFamily(FamliyCoreVo vo) {
 
+        //db
         List<FamilyCoreDto> famliyCoreList = familyCoreDao.getFamliyCoreList(vo);
-        System.out.println("▀▀▀▀▀▀▀▀▀MYSQL查询结果：" + famliyCoreList.size());
+        logger.info("▀▀▀▀▀▀▀▀▀MYSQL查询结果：{}" , famliyCoreList.size());
+
+        //redis
         redisTemplate.opsForValue().set("family"+ DateUtils.getDateTime(), famliyCoreList.toString());
-        System.out.println("▀▀▀▀▀▀▀▀▀REDIS执行成功");
+        logger.info("▀▀▀▀▀▀▀▀▀REDIS执行成功");
+
+
+        // 不同级别的日志
+        logger.info("▀▀▀▀▀▀▀▀日志程序执行开始");
+        logger.trace("This is trace log");
+        logger.debug("This is debug log");
+        logger.info("This is info log");
+        logger.warn("This is warn log");
+        logger.error("This is error log");
+        logger.fatal("This is fatal log");
+
+        // 异常日志
+        try {
+            // some code
+            int a = 1 / 0;
+        } catch (Exception e) {
+            logger.error("An error occurred", e);
+        }
+        logger.info("▀▀▀▀▀▀▀▀日志程序执行完毕");
+
         return famliyCoreList;
 
     }
